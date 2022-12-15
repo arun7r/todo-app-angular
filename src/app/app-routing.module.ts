d131d45fee150140bb41653244afe5ce40fe7129
arun7r <arunrathnakumar97@gmail.com>
2022-12-16 02:07:42 +0530

    todo app using angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,27 @@
-import { NgModule } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { FeaturesComponent } from './features/features.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+import { TodosComponent } from './todos/todos.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'',
+    component:TodosComponent
+  },
+  {
+    path:'instructions',
+    component:InstructionsComponent
+  },
+  {
+    path:'todos',
+    component:TodosComponent
+  },
+  {
+    path:'features',
+    component:FeaturesComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,26 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TodosComponent } from './todos/todos.component';
+import { InstructionsComponent } from './instructions/instructions.component';
+import { FeaturesComponent } from './features/features.component';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TodosComponent,
+    InstructionsComponent,
+    FeaturesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
